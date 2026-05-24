b542b652464508ecef6d79a9c72ce879b951bb4b
llm641 <jw8397938@gmail.com>
2026-05-24 08:46:11 +0800

    connect real ai api

--- main.py
+++ main.py
@@ -1,18 +1,51 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+import httpx
+
+client = OpenAI(
+    api_key=os.getenv("DEEPSEEK_API_KEY"),
+    base_url="https://api.deepseek.com/v1",
+    http_client=httpx.Client(
+        proxy=None,
+        trust_env=False
+    )
+)
+
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
-@app.get("/")
-def read_root():
-    return {"message": "it works!"}
+
 class Message(BaseModel):
     text: str
+
 @app.post("/chat")
 def chat(message: Message):
-    return {"reply": f"你刚刚说了: {message.text}"}
+
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {
+                "role":"user",
+                "content":message.text
+            }
+        ]
+    )
+
+    reply = response.choices[0].message.content
+
+    return {
+        "reply": reply
+    }
